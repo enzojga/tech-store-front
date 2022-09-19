3899ffa86387c2343c6adc585a804c3ab416633e
Enzo <enzo202p@gmail.com>
2022-09-19 15:09:25 -0300

    Conflito main

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,8 +11,8 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function Header() {
 
     const [clicked, setClicked] = useState(false);
-    const { cartItens, setCartItens} = useContext(UserContext);
-  const navigate=useNavigate();
+    const { cartItens, setCartItens } = useContext(UserContext);
+    const navigate = useNavigate();
     console.log(cartItens);
     const { users, setUsers } = useContext(UserContext);
 
@@ -21,40 +21,41 @@ export default function Header() {
         setCartItens([...filteredCartItens])
     }
     console.log(users, '*************')
-    function verifyLogin(){
-        if(users.length===0){
+    function verifyLogin() {
+        if (users.length === 0) {
             toast.error("Você não possui um login :c")
             setTimeout(() => {
                 navigate('/sign-in')
-            }, 2000); 
+            }, 2000);
         }
-         if(users.token!==0){
-            navigate('/checkout')}
+        if (users.token !== 0) {
+            navigate('/checkout')
+        }
     }
-    
+
     let acumulador = 0;
-    cartItens.forEach(t =>  acumulador += Number(t.price));
+    cartItens.forEach(t => acumulador += Number(t.price));
 
     return (
         <>
-            <ToastContainer/>
-        <HeaderStyle>
-                
-            <div>
-                <h2 onClick={() => navigate("/")}>Tech Store</h2>
+            <ToastContainer />
+            <HeaderStyle>
 
                 <div>
-                    <ion-icon name="search-outline"></ion-icon>
-                    <div> { cartItens[0] ? <div><p>{cartItens.length}</p></div> : null} <ion-icon onClick={() => !clicked ? setClicked(true) : setClicked(false)} name="cart-outline"></ion-icon></div>
-                    <ion-icon name="person-circle" onClick={()=>{navigate('/sign-in')}}></ion-icon>
+                    <h2 onClick={() => navigate("/")}>Tech Store</h2>
+
+                    <div>
+                        <ion-icon name="search-outline"></ion-icon>
+                        <div> {cartItens[0] ? <div><p>{cartItens.length}</p></div> : null} <ion-icon onClick={() => !clicked ? setClicked(true) : setClicked(false)} name="cart-outline"></ion-icon></div>
+                        <ion-icon name="person-circle" onClick={() => { navigate('/sign-in') }}></ion-icon>
+                    </div>
                 </div>
-            </div>
-            <div style={{display:clicked === false ? "none" : "initial"}}>
-                <h2>Seu carrinho:</h2>
-                {cartItens[0] ? cartItens.map(p => <div> <img src={p.image}></img> <div><p>{p.name}</p> <p>R$: {(p.price / 100).toFixed(2)}</p></div> <ion-icon onClick={() => removeCartProduct(p)} name="trash"></ion-icon> </div> ) : <h2>Seu carrinho esta vazio</h2>}
-                {cartItens[0] ? <><h2>Total: {(acumulador / 100).toFixed(2)}</h2><Button onClick={verifyLogin}>Finalizar compra</Button></> : null}
-            </div>
-    </HeaderStyle >
-    </>
-)
+                <div style={{ display: clicked === false ? "none" : "initial" }}>
+                    <h2>Seu carrinho:</h2>
+                    {cartItens[0] ? cartItens.map(p => <div> <img src={p.image}></img> <div><p>{p.name}</p> <p>R$: {(p.price / 100).toFixed(2)}</p></div> <ion-icon onClick={() => removeCartProduct(p)} name="trash"></ion-icon> </div>) : <h2>Seu carrinho esta vazio</h2>}
+                    {cartItens[0] ? <><h2>Total: {(acumulador / 100).toFixed(2)}</h2><Button onClick={verifyLogin}>Finalizar compra</Button></> : null}
+                </div>
+            </HeaderStyle >
+        </>
+    )
 }
